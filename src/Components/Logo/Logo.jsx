1fa9142abf6d373283a45e95596933a7e25f1627
JustNormalProgrammer <testkonto1290@gmail.com>
2025-01-28 18:58:33 +0100

    Add navbar styles and animations

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import LogoImg from '../../assets/logo-svgrepo-com.svg'
+import styles from './Logo.module.css'
+export default function Logo(){
+    return (
+        <div className={styles.logoGroup}>Hello</div>
+    )
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import { NavLink } from 'react-router'
+import Logo from '../Logo/Logo'
 import styles from './Navbar.module.css'
 export default function Navbar(){
     return (
-        <nav>
-            <NavLink to={'/'} end>Home</NavLink>
-            <NavLink to={'/shop'} end>Shop</NavLink>
+        <nav className={styles.nav}>
+            <Logo></Logo>
+            <ul>
+                <li>
+                    <NavLink className={styles.link} to={'/'} end>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink className={styles.link}  to={'/shop'} end>Shop</NavLink>
+                </li>
+            </ul>
         </nav>
     )
 }
